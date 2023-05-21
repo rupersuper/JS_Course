bde84135dad13e1fcf668206281cfe900980046d
rupersuper <rupersuper@mail.ru>
2023-05-21 21:13:38 +0600

    упражнение

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-const tasks = ['Tasks 1', 'Tasks 2', 'Tasks 3'];
-console.log(tasks);
-
-for(let i = 0; i < tasks.length; i++) {
-	if (tasks[i] === 'Tasks 2') {
-		continue;
-	}
-	console.log(tasks[i]);
+const arr = ['!', 'JS', 'love', 'I'];
+const resultArray = [];
+for(let i = arr.length; i >= 0; i--) {
+	resultArray.push(arr[i]);
 }
-
-console.log('--------------------------')
-
-for(let i = 0; i < tasks.length; i++) {
-	if (tasks[i] === 'Tasks 2') {
-		break;
-	}
-	console.log(tasks[i]);
-}
+console.log(resultArray.join(''));
