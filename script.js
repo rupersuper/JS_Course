b48052d3eac5a16483e4c7708e73bd5d6f464725
rupersuper <rupersuper@mail.ru>
2023-05-22 23:20:47 +0600

    CyclonCycl

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const arr = ['!', 'JS', 'love', 'I'];
-const resultArray = [];
-for(let i = arr.length; i >= 0; i--) {
-	resultArray.push(arr[i]);
+// for (let i = 1; i < 5; i++) {
+// 	console.log(`Cycl 1 - ${i}`);
+// 	for(let j = 1; j < 5; j++) {
+// 		console.log(`Cycle 2 - ${j}`)
+// 	}
+// }
+
+const tasks = [[1, 'Tasks 1'], [2, 'Tasks 2']];
+
+for (let i = 0; i < tasks.length; i++) {
+	for(let j = 0; j < tasks[i].length; j++) {
+		console.log(tasks[i][j]);
+	}
 }
-console.log(resultArray.join(''));
